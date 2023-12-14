2222c2e38df8d1704b891a6308f98f0996076e00
Bambi på hal is <kronosjo.arvid@live.com>
2023-12-14 23:05:25 +0100

    tset

---
+++ WindowsAuthReverseProxyTest/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WindowsAuthReverseProxyTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Username { get; set; } = "Empty";
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Username = this.HttpContext?.User?.Identity?.Name ?? "Could not read username";
+        }
+    }
+}
